faa5346f75e1bc1d118fbb723d532277b98bc0db
TragodieDesign <brunofernando@tragodiedesign.com.br>
2023-10-09 17:38:08 -0300

    testing get method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000; // Porta do servidor
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
