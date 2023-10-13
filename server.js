cec5a5f7a03b6dab7c3d7f09973dadc4f7b66bfe
TragodieDesign <brunofernando@tragodiedesign.com.br>
2023-10-13 01:26:25 -0300

    Testing company creating

--- server.js
+++ server.js
@@ -20,15 +20,50 @@ app.use(express.json());
 app.post('/rota-de-processamento', async (req, res) => {
   try {
     const token = process.env.BEARER_TOKEN;
-    const url = 'https://api.mspbackups.com/api/Users';
-    const { nome, sobrenome, email } = req.body;
+    const usersUrl = 'https://api.mspbackups.com/api/Users';
+    const companiesUrl = 'https://api.mspbackups.com/api/Companies';
+    const { nome, sobrenome, email, company } = req.body;
 
+    // 1. Valide o e-mail
+    const getUsersResponse = await axios.get(usersUrl, { headers });
+    if (getUsersResponse.status !== 200) {
+      res.status(500).send('Erro ao buscar usuários na API.');
+      return;
+    }
+
+    const users = getUsersResponse.data;
+    const emailExists = users.some(user => user.Email === email);
+
+    if (emailExists) {
+      res.status(200).json({ message: 'Este e-mail já foi cadastrado.', emailExists: true });
+      return;
+    }
+
+    // 2. Crie a empresa
+    const empresaDados = {
+      "Name": `CIAED ${company}`,
+      "StorageLimit": 536870912000,
+      "LicenseSettings": 0,
+      "Destinations": [
+        {
+          "DestinationId": "33b3f17f-5b96-4870-b108-0314d8032e7e"
+        }
+      ]
+    };
+    const empresaResponse = await axios.post(companiesUrl, empresaDados, { headers });
+
+    if (empresaResponse.status !== 200) {
+      res.status(500).send('Erro ao criar a empresa.');
+      return;
+    }
+
+    // 3. Crie o novo usuário
     const dadosParaAPI = {
       "Email": email,
       "FirstName": nome,
       "LastName": sobrenome,
       "NotificationEmails": [email],
-      "Company": "CIAED",
+      "Company": `CIAED ${company}`,
       "Enabled": true,
       "Password": "cadastrociaed123",
       "DestinationList": [
@@ -41,38 +76,16 @@ app.post('/rota-de-processamento', async (req, res) => {
       "SendEmailInstruction": true,
       "LicenseManagmentMode": 0
     };
+    const postResponse = await axios.post(usersUrl, dadosParaAPI, { headers });
 
-    const headers = {
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    };
-
-    const response = await axios.post(url, dadosParaAPI, { headers });
-
-    console.log(response.data);
-
-    const getUsersResponse = await axios.get(url, { headers });
-
-    if (getUsersResponse.status === 200) {
-      const users = getUsersResponse.data;
-
-      const emailExists = users.some(user => user.Email === email);
-
-      if (emailExists) {
-        res.status(200).json({ message: 'Este e-mail já foi cadastrado.', emailExists: true });
-      } else {
-        const postResponse = await axios.post(url, dadosParaAPI, { headers });
-
-        console.log(postResponse.data);
-
-        res.status(200).json({ message: 'Dados enviados com sucesso para a API.', emailExists: false });
-      }
+    if (postResponse.status === 200) {
+      res.status(200).json({ message: 'Dados enviados com sucesso para a API.', emailExists: false });
     } else {
-      res.status(500).send('Erro ao buscar usuários na API.');
+      res.status(500).send('Erro ao criar o novo usuário.');
     }
   } catch (error) {
     console.error('Erro:', error);
-    res.status(500).send('Ocorreu um erro ao enviar os dados para a API.');
+    res.status(500).send('Ocorreu um erro ao processar os dados.');
   }
 });
 
