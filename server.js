d3b7f20c8168a7ef5799951d655093e7ea6f7ccb
TragodieDesign <brunofernando@tragodiedesign.com.br>
2023-10-12 21:42:00 -0300

    solved syntax error

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://www.nublify.com/ciaed");
+  res.header("Access-Control-Allow-Origin", "https://www.nublify.com/ciaed");
   res.header("Access-Control-Allow-Methods", "POST");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
